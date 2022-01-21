1fab57c6d64ba70261a0a1a76342faf4ef8f384d
VladNCode <vldsmrfow@gmail.com>
2022-01-21 09:52:48 +0200

    testing errors

---
+++ typescript.js
@@ -0,0 +1,6 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var arr = [1, 2, 3];
+arr = [1, 2, 'a'];
+console.log(sum(5, 4));
--- typescript.ts
+++ typescript.ts
@@ -2,6 +2,8 @@ const sum = function (a: number, b: number): number {
   return a + b;
 };
 
-let arr: Array<number> = [1, 2, '3'];
+let arr: Array<number> = [1, 2, 3];
+
+arr = [1, 2, 'a'];
 
 console.log(sum(5, 4));
